b07f69187c074fe2f95944a419023995ad37f400
Shivam <panchbhaishivam@gmail.com>
2026-02-03 21:09:18 +0530

    Refactor user_data script for Docker setup
    Updated user_data script to streamline package installation and add ECR login and image pull commands.

--- terraform/launch_template.tf
+++ terraform/launch_template.tf
@@ -17,19 +17,40 @@ resource "aws_launch_template" "docker_lt" {
     }
   }
 
-  user_data = base64encode(<<-EOF
-  #!/bin/bash
-  dnf update -y                 # user_data runs as root so no sudo
-  dnf install -y docker
-  systemctl enable docker
-  systemctl start docker
-  usermod -aG docker ssm-user   # Allow ssm-user to run Docker 
-  dnf install -y git
-  dnf install -y amazon-ssm-agent
-  systemctl enable amazon-ssm-agent
-  systemctl start amazon-ssm-agent
-  EOF
-  )
+ user_data = base64encode(<<-EOF
+#!/bin/bash
+set -e
+
+# Update and install required packages
+dnf update -y
+dnf install -y docker amazon-ssm-agent
+
+# Start services
+systemctl enable docker
+systemctl start docker
+systemctl enable amazon-ssm-agent
+systemctl start amazon-ssm-agent
+
+# Wait for Docker daemon
+until docker info >/dev/null 2>&1; do
+  sleep 2
+done
+
+# Login to ECR (EC2 role is used automatically)
+aws ecr get-login-password --region ap-south-1 \
+  | docker login --username AWS --password-stdin 306991549269.dkr.ecr.ap-south-1.amazonaws.com
+
+# Pull latest app image
+docker pull 306991549269.dkr.ecr.ap-south-1.amazonaws.com/ehr-service:latest
+
+# Run container
+docker rm -f ehr || true
+docker run -d \
+  --name ehr \
+  -p 8000:8000 \
+  306991549269.dkr.ecr.ap-south-1.amazonaws.com/ehr-service:latest
+EOF
+)
 
   tag_specifications {
     resource_type = "instance"

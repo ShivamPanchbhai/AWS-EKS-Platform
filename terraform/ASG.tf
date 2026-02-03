f8351d661f062893f42b114f42776c694a818925
Shivam <panchbhaishivam@gmail.com>
2026-02-03 14:32:36 +0530

    Update ASG.tf

--- terraform/ASG.tf
+++ terraform/ASG.tf
@@ -19,7 +19,7 @@ resource "aws_autoscaling_group" "docker_asg" {
   name = "EC2-ASG"
 
   min_size         = 2
-  max_size         = 2
+  max_size         = 3
   desired_capacity = 2
 
   vpc_zone_identifier = data.aws_subnets.default.ids

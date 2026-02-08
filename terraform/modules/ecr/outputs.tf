7d5a76512062dfef9fb4d1580733a1dcea6c722d
Shivam <panchbhaishivam@gmail.com>
2026-02-08 17:52:16 +0530

    Update outputs.tf

--- terraform/modules/ecr/outputs.tf
+++ terraform/modules/ecr/outputs.tf
@@ -1,5 +1,7 @@
-# ECR repository URL
-# Used by compute to pull Docker images
 output "repository_url" {
-  value = aws_ecr_repository.ehr.repository_url
+  value = data.aws_ecr_repository.ehr.repository_url
+}
+
+output "repository_arn" {
+  value = data.aws_ecr_repository.ehr.arn
 }

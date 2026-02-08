ba3c9445826ee19b2f70c63f1bea636581528c34
Shivam <panchbhaishivam@gmail.com>
2026-02-08 13:37:55 +0530

    Update main.tf

--- terraform/modules/alb/main.tf
+++ terraform/modules/alb/main.tf
@@ -28,3 +28,18 @@ resource "aws_lb_target_group" "this" {
   }
 }
 
+############################################
+        # ALB Listener (HTTP)
+############################################
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.this.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.this.arn
+  }
+}
+
+

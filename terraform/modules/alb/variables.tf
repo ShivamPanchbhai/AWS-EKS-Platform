abc8d63987bf31f4ed72e7ee438b611c949898c9
Shivam <panchbhaishivam@gmail.com>
2026-02-12 21:56:11 +0530

    Update variables.tf

--- terraform/modules/alb/variables.tf
+++ terraform/modules/alb/variables.tf
@@ -1,16 +1,28 @@
+########################################################
+# INPUT VARIABLES
+########################################################
+
+variable "service_name" {
+  description = "Name prefix for ALB resources"
+  type        = string
+}
+
 variable "vpc_id" {
-  type = string
+  description = "VPC where ALB will be deployed"
+  type        = string
 }
 
 variable "subnet_ids" {
-  type = list(string)
-}
-
-variable "service_name" {
-  type = string
+  description = "Public subnets for ALB"
+  type        = list(string)
 }
 
 variable "certificate_arn" {
-  type = string
+  description = "Validated ACM certificate ARN (from ACM module output)"
+  type        = string
 }
 
+variable "domain_name" {
+  description = "Public domain for DNS alias record"
+  type        = string
+}

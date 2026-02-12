ef29204fb969f1e634edbf864e0086595ac1d553
Shivam <panchbhaishivam@gmail.com>
2026-02-12 22:02:24 +0530

    Delete terraform/modules/acm/variables.tf

--- terraform/modules/acm/variables.tf
+++
@@ -1,8 +0,0 @@
-########################################################
-# Variables used in main file
-########################################################
-
-variable "domain_name" {
-  description = "Primary domain for HTTPS certificate"
-  type        = string
-}

ec01ac983b9b3d94f84af4e84d7160a2442c2f41
Shivam <panchbhaishivam@gmail.com>
2026-02-12 22:08:35 +0530

    Create variables.tf

---
+++ terraform/modules/acm/variables.tf
@@ -0,0 +1,8 @@
+########################################################
+# INPUT VARIABLES
+########################################################
+
+variable "domain_name" {
+  description = "Public domain for DNS alias record"
+  type        = string
+}

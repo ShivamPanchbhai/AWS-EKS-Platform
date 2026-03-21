8051783ef43db5f7c491b6867db9315cce9e9b30
Shivam Panchbhai <panchbhaishivam@gmail.com>
2026-03-21 10:26:17 +0530

    Update VPC ID reference in security group resource

--- terraform/modules/compute/lt.tf
+++ terraform/modules/compute/lt.tf
@@ -5,7 +5,7 @@
 ############################################
 resource "aws_security_group" "ec2_sg" {
   name   = "${var.service_name}-ec2-sg"
-  vpc_id = data.aws_vpc.default.id
+  vpc_id = var.vpc_id
 
   # ALB → App
   ingress {

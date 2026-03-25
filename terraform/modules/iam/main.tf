3b22cb15099b291372f7e38a4f75886f3840f55a
Shivam Panchbhai <panchbhaishivam@gmail.com>
2026-03-25 22:31:07 +0530

    Update main.tf

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -46,3 +46,55 @@ resource "aws_iam_role_policy_attachment" "ecs_ec2_role" {
   role       = aws_iam_role.ec2_runtime.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
 }
+
+#############################
+# Prometheus EC2 role (for discovery)
+#############################
+
+resource "aws_iam_role" "prometheus" {
+  name = "prometheus-ec2-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+#############################
+# Instance profile for Prometheus
+#############################
+
+resource "aws_iam_instance_profile" "prometheus" {
+  name = "prometheus-instance-profile"
+  role = aws_iam_role.prometheus.name
+}
+
+#############################
+# Policy → EC2 discovery
+#############################
+
+resource "aws_iam_role_policy" "prometheus_ec2_discovery" {
+  name = "prometheus-ec2-discovery"
+  role = aws_iam_role.prometheus.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ec2:DescribeInstances"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}

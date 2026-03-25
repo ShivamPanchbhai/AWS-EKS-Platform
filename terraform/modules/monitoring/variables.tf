38cfec3061bd8f5d018b33675a77ee5368cfb44e
Shivam Panchbhai <panchbhaishivam@gmail.com>
2026-03-25 18:47:49 +0530

    Update variables.tf

--- terraform/modules/monitoring/variables.tf
+++ terraform/modules/monitoring/variables.tf
@@ -1,5 +1,20 @@
+############################################
+# VPC ID
+# Needed to launch the monitoring EC2 inside
+# the correct network (same VPC as app)
+############################################
 variable "vpc_id" {}
+
+############################################
+# Subnet ID
+# Decides in which subnet the monitoring EC2
+# will be created (usually public/private subnet)
+############################################
 variable "subnet_id" {}
+
+############################################
+# AMI ID
+# Base OS image for the monitoring EC2
+# (Amazon Linux 2023 )
+############################################
 variable "ami_id" {}
-variable "key_name" {}
-variable "app_instance_private_ip" {}

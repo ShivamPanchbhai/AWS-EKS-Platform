4011474a4092beea8490473ac7ea31a3c84c0f2a
Shivam Panchbhai <panchbhaishivam@gmail.com>
2026-03-26 23:35:56 +0530

    Update main.tf

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -115,10 +115,6 @@ output "ec2_runtime_instance_profile_name" {
   value = aws_iam_instance_profile.ec2_runtime.name
 }
 
-output "prometheus_instance_profile_name" {
-  value = aws_iam_instance_profile.prometheus.name
-}
-
 output "prometheus_role_name" {
   value = aws_iam_role.prometheus.name
 }

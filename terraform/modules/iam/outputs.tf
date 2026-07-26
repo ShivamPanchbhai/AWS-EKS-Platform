85af7df833205d4a9ac9754e47ff9dbb6c7bc37d
Shivam Panchbhai <panchbhaishivam@gmail.com>
2026-07-27 01:50:14 +0530

    removing the correct unused variables

--- terraform/modules/iam/outputs.tf
+++ terraform/modules/iam/outputs.tf
@@ -4,17 +4,17 @@
 # e.g. module.iam.eks_cluster_role_arn
 ############################################################
 
-# Legacy EC2 instance profile -- used by existing non-EKS EC2 setup
-output "instance_profile_name" {
-  value = aws_iam_instance_profile.ec2_runtime.name
+# Passed to EKS module -- attached to aws_eks_cluster as the control plane role
+output "eks_cluster_role_arn" {
+  value = aws_iam_role.eks_cluster.arn
 }
 
-# Legacy Prometheus instance profile -- used by existing non-EKS Prometheus setup
-output "prometheus_instance_profile_name" {
-  value = aws_iam_instance_profile.prometheus.name
+# Passed to EKS module -- attached to aws_eks_node_group for worker node permissions
+output "eks_node_group_role_arn" {
+  value = aws_iam_role.eks_node_group.arn
 }
 
-# Legacy Prometheus role name -- used by existing non-EKS Prometheus setup
-output "prometheus_role_name" {
-  value = aws_iam_role.prometheus.name
+# Passed to EKS module -- used by EBS CSI driver addon to manage EBS volumes
+output "ebs_csi_role_arn" {
+  value = aws_iam_role.ebs_csi.arn
 }

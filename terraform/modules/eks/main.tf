a9e7dfd586ba192c37f8328b8eb3022383de6e36
Shivam Panchbhai <panchbhaishivam@gmail.com>
2026-07-27 14:20:29 +0530

    removing the addon_version from modules/eks/main.tf

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -290,6 +290,5 @@ resource "aws_eks_node_group" "monitoring" {
 resource "aws_eks_addon" "ebs_csi" {
   cluster_name             = aws_eks_cluster.this.name
   addon_name               = "aws-ebs-csi-driver"
-  addon_version            = "v1.31.0-eksbuild.1"
   service_account_role_arn = var.ebs_csi_role_arn
 }
